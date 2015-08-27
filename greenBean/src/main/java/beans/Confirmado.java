16bec23e6639f16f59008c0b9fb1f8e7471e6db3
elchata <elchata5@hotmail.com>
2015-08-27 18:06:39 -0300

    confirmado

--- greenBean/src/main/java/beans/Confirmado.java
+++ greenBean/src/main/java/beans/Confirmado.java
@@ -3,13 +3,7 @@ package beans;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
+import javax.persistence.Entity; 
 
 @Entity
 public class Confirmado extends Estado implements Serializable {
